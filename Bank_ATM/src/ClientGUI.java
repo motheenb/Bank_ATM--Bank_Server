0f71b8230c1cd0df5fe0a884ce2426b230d2c6ad
Motheen <74435929+motheenb@users.noreply.github.com>
2021-02-14 23:28:46 -0500

    - First Commit !
    - Add ClientGUI.java, to display window/graphics
    
    - Add fields 'image' & 'graphics' in ClientGUI.java for double buffering
    
    - ClientGUI.java init. JFrame in constructor

---
+++ Bank_ATM/src/ClientGUI.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *
+ * @author Motheen Baig
+ */
+public class ClientGUI extends JFrame {
+
+    private Image image;
+    private Graphics graphics;
+    private final int clientW = 500, clientH = 500;
+
+    public ClientGUI() {
+        setTitle("WorldBank ATM - Motheen Baig");
+        setSize(clientW, clientH);
+        setResizable(false);
+        setVisible(true);
+        image = createImage(clientW, clientH);
+        graphics = image.getGraphics();
+    }
+
+    public void paint(final Graphics g) {
+        if (image == null) {
+            return;
+        }
+        graphics.clearRect(0, 0, clientW, clientH);
+
+        g.drawImage(image, 0, 0, this);
+    }
+
+    public static void log(final Object o) {
+        System.out.println(o);
+    }
+
+    public static void main(final String...args) {
+        new ClientGUI();
+    }
+
+}
