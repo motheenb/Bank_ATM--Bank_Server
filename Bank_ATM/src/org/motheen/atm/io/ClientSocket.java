bdc1471f7a23a9c3195b53abed17033b0782e532
Motheen <74435929+motheenb@users.noreply.github.com>
2021-02-20 03:45:06 -0500

    SecureSession (KeyExchange)
    - Client/Server exchange public key & use DH (Diffie Hellman) generate common key
    - Re-packaged project

--- Bank_ATM/src/org/motheen/atm/io/ClientSocket.java
+++ Bank_ATM/src/org/motheen/atm/io/ClientSocket.java
@@ -1,13 +1,12 @@
 package org.motheen.atm.io;
 
 import org.motheen.atm.ClientGUI;
-import org.motheen.server.ThreadHandler;
+import org.motheen.server.handlers.ThreadHandler;
 
-import javax.xml.crypto.Data;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.security.PublicKey;
+import java.util.concurrent.Future;
 
 /**
  *
@@ -16,8 +15,11 @@ import java.net.Socket;
 public class ClientSocket {
 
     private Socket socket;
+    private Session session = new Session();
     private DataInputStream inputStream;
     private DataOutputStream outputStream;
+    private ObjectInputStream objectInputStream;
+    private ObjectOutputStream objectOutputStream;
 
     public ClientSocket() {
         try {
@@ -25,14 +27,36 @@ public class ClientSocket {
             inputStream = new DataInputStream(socket.getInputStream());
             outputStream = new DataOutputStream(socket.getOutputStream());
             ClientGUI.log("Connected to server.");
+
         } catch (final IOException e) {
             e.printStackTrace();
         }
     }
 
     public void run() {
-        // send client public key
-        // get server public key
+       final Future<Boolean> keyExchange = ThreadHandler.get().submit(this::doKeyExchange);
+       while (!keyExchange.isDone()) {
+           // wait
+       }
+       while (!socket.isClosed()) {
+            // handle incoming
+       }
+    }
+
+    public boolean doKeyExchange() {
+        session.generateKeyPair();
+        try {
+            objectOutputStream = new ObjectOutputStream(outputStream);
+            objectOutputStream.writeObject(session.getPublicKey());
+            objectOutputStream.flush();
+            objectInputStream = new ObjectInputStream(inputStream);
+            session.setOtherPublicKey((PublicKey) objectInputStream.readObject());
+            session.generateSecretKey();
+            ClientGUI.log("Secret Key: " + new String(session.getSecretKey()));
+        } catch (final IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return session.getSecretKey().length > 0;
     }
 
 }
